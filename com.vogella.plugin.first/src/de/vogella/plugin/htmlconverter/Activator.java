dd97d29f47408c8ca863c9ae7bffaa9ee6c3124e
StelmaszczykAdam@gmail.com <StelmaszczykAdam@gmail.com@1bc7f8b6-6d03-2687-9fb5-77ca23c76e9b>
2013-02-23 00:07:09 +0000

    Formatting the code

--- com.vogella.plugin.first/src/de/vogella/plugin/htmlconverter/Activator.java
+++ com.vogella.plugin.first/src/de/vogella/plugin/htmlconverter/Activator.java
@@ -6,17 +6,11 @@ import org.osgi.framework.BundleContext;
 /** The activator class controls the plug-in life cycle */
 public class Activator extends AbstractUIPlugin
 {
-
 	// The plug-in ID
 	public static final String PLUGIN_ID = "de.vogella.plugin.htmlconverter";
 
 	// The shared instance
 	private static Activator plugin;
-
-	/** The constructor */
-	public Activator()
-	{
-	}
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext) */
